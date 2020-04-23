b5c978ecff662f6326af19cdf4a8cc10e9adfe68
ayushgarg27 <64222620+ayushgarg27@users.noreply.github.com>
2020-04-24 00:08:38 +0530

    Add files via upload

---
+++ pattern3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    for(int i=n; i>=1; i--){
+        for(int j=1; j<=i; j++){
+            printf("*");
+            printf(" ");
+        }
+        printf("\n");
+    }
+}
